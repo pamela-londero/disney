5711d224b6ce5ed038259af072d41cb47ba431bd
Pamela <pamelalondero@gmail.com>
2022-04-08 09:42:52 -0300

    Initial commit

---
+++ src/test/java/com/alkemy/disney/films/FilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
